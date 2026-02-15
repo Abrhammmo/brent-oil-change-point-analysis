f061e6944a368db17ffd2d50f08fc3cfdf86bf70
Abrhammo <abrhammolla4@gmail.com>
2026-02-15 12:15:47 +0300

    top-level package docstring.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,1 +1,4 @@
-"""src.data package init"""
+"""Data package exports."""
+
+from src.data.load_data import load_brent_data, load_events, load_prices
+from src.data.macro_loader import load_macro_data
